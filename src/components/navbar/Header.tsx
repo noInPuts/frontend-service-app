5bc29c6e8ca22ec09c0bf14430963117a2fe4435
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-11-29 17:15:22 +0100

    Changed to HTTPS and url path for api

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -15,7 +15,7 @@ export default function Header({ isLoggedIn } : { isLoggedIn : Boolean }) {
 
     const logout = async () => {
         try {
-            const response = await fetch(`${backendUrl}:8086/api/user/logout`, {
+            const response = await fetch(`${backendUrl}user/logout`, {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
@@ -94,7 +94,7 @@ export default function Header({ isLoggedIn } : { isLoggedIn : Boolean }) {
         }
 
         try {
-            const response = await fetch(`${backendUrl}:8086/api/user/login`, {
+            const response = await fetch(`${backendUrl}user/login`, {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
